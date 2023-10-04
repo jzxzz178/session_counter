7894ec9fe0088c272cdae358444e41a5e9a572e9
DESKTOP-FEDOR <pf2772@gmail.com>
2023-10-04 17:12:54 +0500

    working version

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+from datetime import datetime
+from uuid import uuid1
+
 import fastapi
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from pydantic import ValidationError
+
 from pages.router import router as router_pages
 from fastapi.staticfiles import StaticFiles
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-from sql_app import crud, models, schemas, database
-from sql_app.database import SessionLocal, engine
+# from sql_app import crud, models, schemas, database
+# from sql_app.database import SessionLocal, engine
 
 from session_counter import session_counter
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="My Website"
@@ -24,6 +29,19 @@ app.mount('/static', StaticFiles(directory='static'), name='static')
 
 
 @app.get("/")
-@session_counter
-async def root(request: fastapi.Request, db: Session = Depends(database.get_db)):
+# @session_counter
+def root(request: fastapi.Request):
+
+    # user_session = schemas.CreateUserSession(
+    #     id=str(uuid1()),
+    #     ip=request.client.host,
+    #     datetime=datetime.now(),
+    #     path=request.url.path
+    # )
+    #
+    # crud.create_user_session(
+    #     db,
+    #     user_session
+    # )
+    # db_session = crud.create_user_session(db, session_create)
     return RedirectResponse('pages/home')
--- pages/router.py
+++ pages/router.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
-from sql_app import database
-from sql_app.database import SessionLocal, engine
+# from sql_app import database
 
-from session_counter import session_counter
+# from session_counter import session_counter
+# from sql_app import crud, models, schemas, database
 
 router = APIRouter(
     prefix='/pages',
@@ -16,18 +16,18 @@ templates = Jinja2Templates(directory='templates')
 
 
 @router.get('/base')
-@session_counter
-def get_base_template(request: Request, db: Session = Depends(database.get_db)):
+# @session_counter
+def get_base_template(request: Request):
     return templates.TemplateResponse('base.html', {'request': request})
 
 
 @router.get('/search')
-@session_counter
-def get_search_template(request: Request, db: Session = Depends(database.get_db)):
+# @session_counter
+def get_search_template(request: Request):
     return templates.TemplateResponse('search.html', {'request': request})
 
 
 @router.get('/home')
-@session_counter
-def get_home_page(request: Request, db: Session = Depends(database.get_db)):
+# @session_counter
+def get_home_page(request: Request):
     return templates.TemplateResponse('home.html', {'request': request})
